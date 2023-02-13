99689d3fe3d0b4aa4935fd6c78d29375775b0db3
Rushaan Gandhi <rushaansps@gmail.com>
2023-02-13 16:28:32 +0530

    files and folders created

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import { CTA, Brand, Navbar} from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
