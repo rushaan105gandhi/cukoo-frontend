9b0475d66a2f4d0e83fc377e97ab6ed5080475ff
Rushaan Gandhi <rushaansps@gmail.com>
2023-02-23 23:19:55 +0530

    header added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,12 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
     </div>
   )
 }
