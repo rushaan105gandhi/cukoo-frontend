efff7efff81ffee78b6fd1d121f7984abf0392c5
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-25 15:30:35 +0530

    Navbar changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,30 +16,30 @@ const Menu = () => (
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
+    <div className='cukoo__navbar'>
+      <div className='cukoo__navbar-links'>
+        <div className='cukoo__navbar-links_logo'>
           <img src={logo} alt="logo" />
           {/* <img src="logos.pnj" alt="logo" /> */}
         </div>
-        <div className='gpt3__navbar-links_container'>
+        <div className='cukoo__navbar-links_container'>
           <Menu />
         </div>
       </div>
-      <div className='gpt3__navbar-sign'>
+      <div className='cukoo__navbar-sign'>
         <p>Sign in</p>
         <button type='button'>Sign up</button>
       </div>
-      <div className='gpt3__navbar-menu'>
+      <div className='cukoo__navbar-menu'>
         {toggleMenu
           ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-          <div className='gpt3__navbar-menu_container scale-up-center'>
-            <div className='gpt3__navbar-menu_container-links'>
+          <div className='cukoo__navbar-menu_container scale-up-center'>
+            <div className='cukoo__navbar-menu_container-links'>
               <Menu />
-              <div className='gpt3__navbar-menu_container-links-sign'>
+              <div className='cukoo__navbar-menu_container-links-sign'>
               <p>Sign in</p>
               <button type='button'>Sign up</button>
               </div>
