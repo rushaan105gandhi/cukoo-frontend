876effcb4a737ac7afef335db13557b77ad6054d
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-25 16:09:11 +0530

    header and navbar changed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,20 +7,20 @@ import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='home'>
-      <div className='gpt3__header-content'>
+    <div className='cukoo__header section__padding' id='home'>
+      <div className='cukoo__header-content'>
         <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercises blessing indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-        <div className='gpt3__header-content__input'>
+        <div className='cukoo__header-content__input'>
           <input type="email" placeholder='Your email Adress' />
           <button type='button'>Get Started</button>
         </div>
-        <div className='gpt3__header-content__people'>
+        <div className='cukoo__header-content__people'>
           <img src={people} alt="people" />
           <p>1600 people requested access in last 24 hours.</p>
         </div>
       </div>
-        <div className='gpt3__header-image'>
+        <div className='cukoo__header-image'>
           <img src={ai} alt="ai" />
         </div>
       
